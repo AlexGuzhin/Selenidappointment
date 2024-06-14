ebede2ab5f7786e7d9cc4ed6cca348724fbe1416
Alex Guzhin <growfood1989@yandex.ru>
2024-06-14 17:56:37 +0300

    Создание проекта

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,32 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTaskOneTest {
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id='city']input").setValue("Рязань");
+        String planningDate = generateDate(4, "dd.mm.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
+        $("[data-test-id='phone'] input").setValue("+78005553535");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible,Duration.ofSeconds(15)).shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
+    }
+}
