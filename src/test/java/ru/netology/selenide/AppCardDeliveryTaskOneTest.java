01aa2c3545044b3cfb764b6cf1d7356f610aa704
Alex Guzhin <growfood1989@yandex.ru>
2024-06-14 18:37:37 +0300

    Доделывание

--- src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -19,7 +19,7 @@ public class AppCardDeliveryTaskOneTest {
     @Test
     public void shouldBeSuccessfullyCompleted() {
         open("http://localhost:9999");
-        $("[data-test-id='city']input").setValue("Рязань");
+        $("[data-test-id='city']input").setValue("Омск");
         String planningDate = generateDate(4, "dd.mm.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
@@ -27,6 +27,8 @@ public class AppCardDeliveryTaskOneTest {
         $("[data-test-id='phone'] input").setValue("+78005553535");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content").shouldBe(Condition.visible,Duration.ofSeconds(15)).shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
+        $(".notification__content")
+                .shouldBe(Condition.visible,Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 }
